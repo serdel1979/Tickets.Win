f39dd587de486550d53cde6dc1eb36363dc15476
sergio <sdlbsso@gmail.com>
2024-06-14 12:57:36 -0300

    app de tickets windows

---
+++ TicketApp/Login.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace TicketApp
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtUser = new TextBox();
+            txtPassword = new TextBox();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(181, 119);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Usuario";
+            // 
+            // txtUser
+            // 
+            txtUser.Location = new Point(259, 116);
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(214, 23);
+            txtUser.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(259, 176);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(214, 23);
+            txtPassword.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(181, 176);
+            label2.Name = "label2";
+            label2.Size = new Size(36, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Clave";
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(695, 388);
+            Controls.Add(txtPassword);
+            Controls.Add(label2);
+            Controls.Add(txtUser);
+            Controls.Add(label1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtUser;
+        private TextBox txtPassword;
+        private Label label2;
+    }
+}
